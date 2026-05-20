ef3f13bf652b0f2060e03a23ab4c4efd1c03e396
RondneyLoiola <rondney.loiola11@gmail.com>
2026-05-20 18:59:37 -0300

    correção problema lib/prisma.ts, e login com github

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,12 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: AUTH_SECRET! */
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+	secret: process.env.AUTH_SECRET!,
+	trustHost: true,
 	adapter: PrismaAdapter(prisma),
 	providers: [GitHub],
 });
