47b4d7349abd3a1d058f0ac92a5185ead442f1e2
RondneyLoiola <rondney.loiola11@gmail.com>
2026-05-20 19:11:42 -0300

    tipagem next-auth para adicionar bio e username no user authenticado

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import type { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+	interface Session {
+		user: DefaultSession["user"];
+	}
+
+	interface User {
+		id: string;
+		name?: string;
+		email?: string;
+		userName?: string;
+		bio?: string;
+	}
+}
