54c3e8d387a572070c73ea3bbbda4180ab1336ee
RondneyLoiola <rondney.loiola11@gmail.com>
2026-05-23 20:13:52 -0300

    formatando os valores e colocando-os nos cards de stats

--- src/app/dashboard/_components/analytics.tsx
+++ src/app/dashboard/_components/analytics.tsx
@@ -1,4 +1,5 @@
 import { DollarSign, Users, Wallet } from "lucide-react";
+import { formatCurrency } from "@/utils/format";
 import { getStats } from "../_data-access/get-stats-creator";
 import { StatCard } from "./stats-card";
 
@@ -9,9 +10,10 @@ export async function Stats({
 	userId: string;
 	stripeAccountId: string;
 }) {
-	const data = await getStats(userId, stripeAccountId);
-
-	console.log(data)
+	const { balance, totalAmountDonated, totalDonations } = await getStats(
+		userId,
+		stripeAccountId,
+	);
 
 	return (
 		<div className="grid gap-6 grid-cols-1 md:grid-cols-3 mb-6">
@@ -19,23 +21,22 @@ export async function Stats({
 				title="Apoiadores"
 				description="Total de apoiadores"
 				icon={<Users className="w-8 h-8 text-blue-400" />}
-				value={0}
+				value={totalDonations ?? 0}
 			/>
 
 			<StatCard
 				title="Total recebido"
 				description="Quantidade total recebida"
 				icon={<DollarSign className="w-8 h-8 text-amber-500" />}
-				value={0}
+				value={formatCurrency((totalAmountDonated ?? 0) / 100)}
 			/>
 
 			<StatCard
 				title="Saldo em conta"
 				description="Saldo pendente"
 				icon={<Wallet className="w-8 h-8 text-green-500" />}
-				value={0}
+				value={formatCurrency((balance ?? 0) / 100)}
 			/>
 		</div>
 	);
 }
-  
